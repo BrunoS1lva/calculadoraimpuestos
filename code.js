f66da72fdd3bc201eff9e13d3840fd971ffdeed1
Bruno Silva <soy.brunosilva@gmail.com>
2023-06-07 12:53:53 -0300

    Primer commit añadiendo las cosas al repo

---
+++ code.js
@@ -0,0 +1,15 @@
+
+let btn = document.getElementById('btn-calcular');
+btn.addEventListener('click', calculo);
+
+function calculo(){
+    let inputDinero = document.getElementById('input-dinero');
+    let money = inputDinero.value;
+    let impuesto1 = money*45/100;
+    let impuesto2 = money*30/100;
+    let result = +money+impuesto1+impuesto2;
+    alert(`Debes ${result} de dinero pa`);
+}
+
+//Impuesto 1 = Adelanto al impuesto a las Ganancias y los Bienes Personales 45%
+//Impuesto2 = Impuesto pais 30%
